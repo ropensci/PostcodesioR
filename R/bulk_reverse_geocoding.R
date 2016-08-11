33e1b61e37145abb3e1483db338293f1ef9e3163
erzk <e.walczak@hotmail.com>
2016-08-11 10:34:51 +0100

    first commit - not functional
    passing the list needs fixing

---
+++ R/bulk_reverse_geocoding.R
@@ -0,0 +1,23 @@
+#' Reverse geocoding - NOT WORKING
+#'
+#' Returns nearest postcodes for a given longitude and latitude. Accepts up to 100 geolocations.
+#'
+#' This method requires a JSON object containing an array of geolocation objects to be POSTed.
+#' Each geolocation object accepts an optional radius (meters) and limit argument.
+#' Both default to 100m and 10 respectively. It also accepts a wideSearch argument.
+#'
+#' @param A list containing an array of geolocation objects.
+#'
+#' @return A list with available data.
+#'
+#' @examples
+#' bulk_reverse_geocoding()
+
+bulk_reverse_geocoding <- function(longitude, latitude) {
+  r <- GET(paste0("https://api.postcodes.io/postcodes?lon=",
+                  longitude,
+                  "&lat=",
+                  latitude))
+  warn_for_status(r)
+  content(r)
+}
