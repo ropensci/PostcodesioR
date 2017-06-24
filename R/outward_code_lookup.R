219e6ae9da8ce57c5625b979d5c544dba07b63b4
Eryk Walczak <e.walczak@hotmail.com>
2017-06-24 22:47:36 +0100

    format

--- R/outward_code_lookup.R
+++ R/outward_code_lookup.R
@@ -1,16 +1,22 @@
 #' Outward code lookup
 #'
 #' Geolocation data for the centroid of the outward code specified.
-#' The outward code represents the first half of any postcode (separated by a space).
 #'
-#' @param outcode A string.
+#' @import httr
+#'
+#' @param outcode A string. The outward code representing the first half of any postcode (separated by a space).
+#'
+#' @export
+#'
 #' @return The list of geographical properties.
 #'
 #' @examples
 #' outward_code_lookup("E1")
-#' @export
-
+#'
 outward_code_lookup <- function(outcode) {
+  if (!is.character(outcode) || nchar(outcode) < 2) {
+    stop("Please provide a valid UK outcode.")
+  }
   r <- GET(paste0("https://api.postcodes.io/outcodes/", outcode))
   warn_for_status(r)
   content(r)
