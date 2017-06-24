fee33b5cdd485a9f2f22d4555fd8d846fe91443a
Eryk Walczak <e.walczak@hotmail.com>
2017-06-24 22:47:16 +0100

    format

--- R/outcode_reverse_geocoding.R
+++ R/outcode_reverse_geocoding.R
@@ -8,20 +8,32 @@
 #'
 #' radius= (not required) Limits number of postcodes matches to return. Defaults to 5,000m. Needs to be less than 25,000m.
 #'
-#' @param longitude A string. Needs to have at least two decimal points.
-#' @param latitude A string. Needs to have at least two decimal points.
+#' @import httr
+#'
+#' @param longitude A string, integer or float. Needs to have at least two decimal points.
+#' @param latitude A string, integer or float. Needs to have at least two decimal points.
+#'
+#' @export
+#'
 #' @return A list of geographical properties.
 #'
 #' @examples
 #' outcode_reverse_geocoding("-3.15", "51.47")
+#' outcode_reverse_geocoding(-3.15, 51.47)
 #' outcode_reverse_geocoding("-3.15807731271522", "51.4799900627036")
-#' @export
-
+#'
 outcode_reverse_geocoding <- function(longitude, latitude) {
+  if (nchar(longitude) < 4) {
+    stop("Please provide a valid longitude with at least two decimal points.")
+  }
+  if (nchar(latitude) < 4) {
+    stop("Please provide a valid latitude with at least two decimal points.")
+  }
   r <- GET(paste0("https://api.postcodes.io/outcodes?lon=",
                   longitude,
                   "&lat=",
                   latitude))
+  # TODO add optional parameters
   warn_for_status(r)
   content(r)
 }
