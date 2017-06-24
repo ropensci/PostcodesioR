946a9a55ee03012b630be9f859c2fa195a176aa0
Eryk Walczak <e.walczak@hotmail.com>
2017-06-25 00:37:14 +0100

    first commit

--- tests/testthat/test-postcode_autocomplete.R
+++ tests/testthat/test-postcode_autocomplete.R
@@ -1,5 +1,20 @@
 context("postcode_autocomplete")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("postcode_autocomplete works as expected", {
+  incorrect_postcode <- 1
+  incorrect_limit1 <- 101
+  incorrect_limit2 <- "11"
+  correct_limit = 11
+
+  postcode_autocomplete_result1 <- postcode_autocomplete("E1")
+  postcode_autocomplete_result2 <- postcode_autocomplete("E1", limit = correct_limit)
+
+  expect_error(postcode_autocomplete(incorrect_postcode))
+  expect_error(postcode_autocomplete("E1", limit = incorrect_limit1))
+  expect_error(postcode_autocomplete("E1", limit = incorrect_limit2))
+
+  expect_that(postcode_autocomplete_result1, is_a("list"))
+  expect_that(postcode_autocomplete_result2, is_a("list"))
+
+  expect_equal(length(postcode_autocomplete_result2$result), correct_limit)
 })
