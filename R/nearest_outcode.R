1b3a4cd6fc14b89587c29f5084bd2b6e144e9fd1
Eryk Walczak <e.walczak@hotmail.com>
2017-06-24 18:23:50 +0100

    works. no optional parameters

--- R/nearest_outcode.R
+++ R/nearest_outcode.R
@@ -6,16 +6,24 @@
 #'
 #' radius= (not required) Limits number of postcodes matches to return. Defaults to 5,000m. Needs to be less than 25,000m.
 #'
-#' @param postcode A string.
+#' @import httr
+#'
+#' @param outcode A string with a UK postcode.
+#'
+#' @export
+#'
 #' @return A list of geographical properties.
 #'
 #' @examples
-#' nearest_postcode("EC1Y 8LX")
-#' nearest_postcode("EC1Y8LX")
-#' @export
-
-nearest_postcode <- function(postcode) {
-  r <- GET(paste0("https://api.postcodes.io/postcodes/", postcode, "/nearest"))
+#' nearest_outcode("EC1Y")
+#'
+nearest_outcode <- function(outcode, limit = NULL, radius = NULL) {
+  if (!is.character(outcode) || nchar(outcode) < 2) {
+    stop("Please provide a valid UK outcode.")
+  }
+  r <- GET(paste0("https://api.postcodes.io/outcodes/", outcode, "/nearest"))
+  # r <- GET("https://api.postcodes.io/outcodes",
+  #          query = list(outcode = outcode, limit = limit, radius = radius))
   warn_for_status(r)
   content(r)
 }
