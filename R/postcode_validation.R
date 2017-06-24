e727994a60efddac3c5c85f475683de8ee4be46f
Eryk Walczak <e.walczak@hotmail.com>
2017-06-25 00:32:58 +0100

    format and examples

--- R/postcode_validation.R
+++ R/postcode_validation.R
@@ -2,13 +2,18 @@
 #'
 #' Convenience method to validate a postcode.
 #'
-#' @param postcode A string.
+#' @import httr
+#'
+#' @param postcode A string. Valid UK postcode.
+#'
+#' @export
+#'
 #' @return A list consisting of the HTTP response, and True or False (meaning valid or invalid respectively).
 #'
 #' @examples
-#' postcode_validation("EC1Y 8LX")
-#' @export
-
+#' postcode_validation("EC1Y 8LX") # returns TRUE
+#' postcode_validation("XYZ") # returns FALSE
+#'
 postcode_validation <- function(postcode) {
   r <- GET(paste0("https://api.postcodes.io/postcodes/", postcode, "/validate"))
   warn_for_status(r)
