93481bb91ad4cd54a18d5324151d6efe539fdcd9
Eryk Walczak <e.walczak@hotmail.com>
2017-06-25 00:32:10 +0100

    format and validation

--- R/postcode_lookup.R
+++ R/postcode_lookup.R
@@ -2,15 +2,22 @@
 #'
 #' Lookup a postcode.
 #'
-#' @param postcode A string.
+#' @import httr
+#'
+#' @param postcode A string. Valid UK postcode.
+#'
+#' @export
+#'
 #' @return A list. Returns all available data if found. Returns 404 if postcode does not exist.
 #'
 #' @examples
 #' postcode_lookup("EC1Y8LX")
 #' postcode_lookup("EC1Y 8LX")
-#' @export
-
+#'
 postcode_lookup <- function(postcode) {
+  if (!is.character(postcode) || nchar(postcode) < 2) {
+    stop("Please provide a valid UK outcode.")
+  }
   r <- GET(paste0("https://api.postcodes.io/postcodes/", postcode))
   warn_for_status(r)
   content(r)
