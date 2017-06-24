9bea286d70debb179e2a358252a431b101ae75fe
Eryk Walczak <e.walczak@hotmail.com>
2017-06-24 18:21:07 +0100

    format

--- R/bulk_postcode_lookup.R
+++ R/bulk_postcode_lookup.R
@@ -2,13 +2,18 @@
 #'
 #' Returns a list of matching postcodes and respective available data.
 #'
+#' @import httr
+#'
 #' @param postcodes Accepts a list of postcodes. Accepts up to 100 postcodes.
+#'
+#' @return A list.
+#'
+#' @export
 #'
 #' @examples
 #' pc_list <- list(postcodes = c("PR3 0SG", "M45 6GN", "EX165BL"))
 #' bulk_postcode_lookup(pc_list)
-#' @export
-
+#'
 bulk_postcode_lookup <- function(postcodes) {
   if (!is.list(postcodes))
     stop("Please provide a list with postcodes.")
