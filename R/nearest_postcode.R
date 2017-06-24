c4853bb950b6b198ec84e62e50613d5051c69b58
Eryk Walczak <e.walczak@hotmail.com>
2017-06-24 22:47:04 +0100

    format

--- R/nearest_postcode.R
+++ R/nearest_postcode.R
@@ -7,15 +7,23 @@
 #' limit= (not required) Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
 #'
 #' radius= (not required) Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+#' @import httr
+#'
 #' @param postcode A string.
+#'
+#' @export
+#'
 #' @return A list of geographic properties of the nearest postcode.
 #'
 #' @examples
 #' nearest_postcode("EC1Y 8LX")
-#' @export
-
+#'
 nearest_postcode <- function(postcode) {
+  if (!is.character(postcode) || nchar(postcode) < 2) {
+    stop("Please provide a valid UK postcode.")
+  }
   r <- GET(paste0("https://api.postcodes.io/postcodes/", postcode, "/nearest"))
+  # TODO add optional parameters
   warn_for_status(r)
   content(r)
 }
