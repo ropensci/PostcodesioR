71bf7b22bcef6f7531ba448de92ce80a220189f3
Eryk Walczak <e.walczak@hotmail.com>
2017-06-25 00:33:28 +0100

    optional parameters

--- R/reverse_geocoding.R
+++ R/reverse_geocoding.R
@@ -2,24 +2,27 @@
 #'
 #' Returns nearest postcodes for a given longitude and latitude.
 #'
-#' Optional Query Parameters:
-#' limit= (not required) Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
-#' radius= (not required) Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
-#' wideSearch= (not required) Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we've created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+#' @import httr
 #'
 #' @param longitude A string. Needs to have at least three decimal points.
 #' @param latitude A string. Needs to have at least three decimal points.
+#' @param limit An integer. Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+#' @param radius An integer. Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+#' @param wideSearch TRUE or FALSE. Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we've created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+#'
 #' @return A list with available data.
 #'
-#' @examples
-#' reverse_geocoding("-3.158", "51.479")
 #' @export
-
-reverse_geocoding <- function(longitude, latitude) {
-  r <- GET(paste0("https://api.postcodes.io/postcodes?lon=",
-                  longitude,
-                  "&lat=",
-                  latitude))
+#'
+#' @examples
+#' reverse_geocoding(0.127, 51.507)
+#' reverse_geocoding("0.1275", "51.5073", limit = 3)
+#' reverse_geocoding("0.1275", "51.5073", limit = 11, radius = 200)
+#'
+reverse_geocoding <- function(longitude, latitude, limit = 10, radius = 100, wideSearch = NULL) {
+  r <- GET("https://api.postcodes.io/postcodes",
+           query = list(lon = longitude, lat = latitude,
+                        limit = limit, radius = radius, wideSearch = wideSearch))
   warn_for_status(r)
   content(r)
 }
