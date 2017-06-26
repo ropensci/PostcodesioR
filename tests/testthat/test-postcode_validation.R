5ec3d610f62d7d427c4c77c506c877b4509d4f59
Eryk Walczak <e.walczak@hotmail.com>
2017-06-26 22:32:20 +0100

    list to logical

--- tests/testthat/test-postcode_validation.R
+++ tests/testthat/test-postcode_validation.R
@@ -6,8 +6,8 @@ test_that("postcode_validation works as expected", {
   postcode_validation_result3 <- postcode_validation("E")
   postcode_validation_result4 <- postcode_validation(1)
 
-  expect_true(postcode_validation_result1$result)
-  expect_false(postcode_validation_result2$result)
-  expect_false(postcode_validation_result3$result)
-  expect_false(postcode_validation_result4$result)
+  expect_true(postcode_validation_result1)
+  expect_false(postcode_validation_result2)
+  expect_false(postcode_validation_result3)
+  expect_false(postcode_validation_result4)
 })
