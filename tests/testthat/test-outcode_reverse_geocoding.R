7665e61ec276696683d9dca066c2e53f50c5b147
Eryk <e.walczak@hotmail.com>
2018-01-10 23:33:20 +0000

    new tests

--- tests/testthat/test-outcode_reverse_geocoding.R
+++ tests/testthat/test-outcode_reverse_geocoding.R
@@ -4,13 +4,16 @@ test_that("outcode_reverse_geocoding works as expected", {
   incorrect_geocode1 <- 3.1
   incorrect_geocode2 <- "4"
 
-  outcode_reverse_geocoding_result <- outcode_reverse_geocoding("-3.15", "51.47")
+  outcode_reverse_geocoding_result <-
+    outcode_reverse_geocoding("-3.15", "51.47")
 
   expect_error(outcode_reverse_geocoding(incorrect_geocode1))
   expect_error(outcode_reverse_geocoding(incorrect_geocode2))
-  expect_error(outcode_reverse_geocoding(outcode_reverse_geocoding(-3.15, 51.47, limit = 101, radius = 20000)))
-  expect_error(outcode_reverse_geocoding(outcode_reverse_geocoding(-3.15, 51.47, limit = 11, radius = 3000)))
-  expect_error(outcode_reverse_geocoding(outcode_reverse_geocoding(-3.15, 51.47, limit = 11, radius = 25001)))
+  expect_error(outcode_reverse_geocoding(-3, 51.47))
+  expect_error(outcode_reverse_geocoding(-3.15, 51))
+  expect_error(outcode_reverse_geocoding(-3.15, 51.47, limit = 101, radius = 20000))
+  expect_error(outcode_reverse_geocoding(-3.15, 51.47, limit = 11, radius = 3000))
+  expect_error(outcode_reverse_geocoding(-3.15, 51.47, limit = 11, radius = 25001))
 
   expect_that(outcode_reverse_geocoding_result, is_a("list"))
 })
--- tests/testthat/test-place_lookup.R
+++ tests/testthat/test-place_lookup.R
@@ -5,4 +5,6 @@ test_that("place_lookup works as expected", {
   place_lookup_result <- place_lookup("osgb4000000074544700")
 
   expect_that(place_lookup_result, is_a("list"))
+
+  expect_error(place_lookup(1))
 })
