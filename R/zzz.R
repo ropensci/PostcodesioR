275b0fa750351cea66b736ca935887b2024f698c
Eryk <e.walczak@hotmail.com>
2018-04-22 17:41:14 +0100

    user_agent removed

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 #' @importFrom httr GET
-#' @importFrom httr user_agent
 #' @importFrom httr warn_for_status
 #' @importFrom httr status_code
 #' @importFrom httr content
-ua <- httr::user_agent("http://github.com/erzk/PostcodesioR")
 
 extract_results <- function(x) {
   httr::warn_for_status(x)
