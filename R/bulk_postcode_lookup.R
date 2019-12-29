e10eaaf813c4dac1e8283f26ca8eaff6f03520c8
Layik Hama <layik.hama@gmail.com>
2019-12-29 23:14:59 +0000

    Minor, correct message

--- R/bulk_postcode_lookup.R
+++ R/bulk_postcode_lookup.R
@@ -30,12 +30,12 @@ bulk_postcode_lookup <- function(postcodes) {
 check_list_limit <- function(x) {
   if (!is.list(x))
     stop("Please provide a list with postcodes.")
+  if (length(x) == 0)
+    stop("Please provide a list with more than one postcode")
   # assuming httr::body_config which relies on jsonlite::toJSON
   # which parses all list values
   # https://github.com/jeroen/jsonlite/blob/
   # 71e2b3ab96dd8b12260b22d9a256865e59bf8351/R/asJSON.list.R
-  if (length(x) == 0)
-    stop("Please provide a list with at least one postcode")
   count <- sum(sapply(x, length))
   if (count > 100)
     stop("Please prrovide a list with less than 100 postcodes.")
