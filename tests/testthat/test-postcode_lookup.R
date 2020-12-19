815cbc00b2bf767be3de1c8b5583a8d91358254f
erzk <erykjwalczak@gmail.com>
2020-12-19 19:52:22 +0000

    test if new columns added to API

--- tests/testthat/test-postcode_lookup.R
+++ tests/testthat/test-postcode_lookup.R
@@ -15,4 +15,18 @@ test_that("postcode_lookup works as expected", {
   expect_error(postcode_lookup(incorrect_postcode3))
 
   expect_that(postcode_lookup_result, is_a("data.frame"))
+
+  # Check if new variables were added to the API
+  harcoded_lookup_names <-
+    c("postcode", "quality", "eastings", "northings", "country",
+      "nhs_ha", "longitude", "latitude", "european_electoral_region",
+      "primary_care_trust", "region", "lsoa", "msoa", "incode", "outcode",
+      "parliamentary_constituency", "admin_district", "parish", "admin_county",
+      "admin_ward", "ced", "ccg", "nuts", "admin_district_code",
+      "admin_county_code", "admin_ward_code", "parish_code",
+      "parliamentary_constituency_code", "ccg_code", "ccg_id_code",
+      "ced_code", "nuts_code", "lsoa_code", "msoa_code", "lau2_code")
+  postcode_lookup_names <- names(postcode_lookup_result)
+
+  expect_equal(harcoded_lookup_names, postcode_lookup_names)
 })
